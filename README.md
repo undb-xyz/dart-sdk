e6782f9245e40210279680eb70817dd3e678f40b
nichenqin <nichenqin@hotmail.com>
2023-11-19 17:29:28 +0800

    feat: read from .env file

--- README.md
+++ README.md
@@ -8,6 +8,14 @@ dart pub get
 
 ## Test
 
+Copy `.env` file from `.env.test.example` and fill `API_KEY` environment
+
+```bash
+cp .env.test.example .env
+```
+
+Run test
+
 ```bash
 dart test
 ```
--- test/dart_sdk_test.dart
+++ test/dart_sdk_test.dart
@@ -1,9 +1,18 @@
 import 'package:dart_sdk/dart_sdk.dart';
 import 'package:test/test.dart';
+import 'package:dotenv/dotenv.dart';
 
 void main() {
-  group('A group of tests', () {
-    final undb = Undb("wKqt2g6MfrH68crJK7dPGs05lwc2NYVL3omiXRnS");
+  var env = DotEnv()..load();
+
+  group('Test undb', () {
+    var apiKey = env['API_KEY'];
+
+    if (apiKey == null) {
+      fail('环境变量 API_KEY 不存在或为空，测试终止。');
+    }
+
+    final undb = Undb(apiKey);
 
     setUp(() {
       // Additional setup goes here.
